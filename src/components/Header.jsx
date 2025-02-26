1d7f2bee01d77ab82e0a78e9a7de6c19059a476b
oztuzelosman <oztuzelosman@gmail.com>
2025-02-26 14:45:50 +0300

    Adding Clerk Header
    Creating Sign-in and Sign-up pages with clerk.

---
+++ src/components/Header.jsx
@@ -0,0 +1,31 @@
+import Link from 'next/link';
+import {SignedIn,SignedOut,UserButton} from '@clerk/nextjs'
+
+export default function Header(){
+  return(
+    <div className='flex justify-between items-center p-3 max-w-6xl mx-auto'>
+      <ul className='flex gap-4'>
+        <SignedIn>
+          <UserButton/>
+        </SignedIn>
+        <SignedOut>
+          <Link href={'/sign-in'}>Sign In</Link>
+        </SignedOut>
+        <li className="hidden sm:block">
+          <Link href={'/'}>Home</Link>
+        </li>
+        <li className="hidden sm:block">
+          <Link href={'/about'}>About</Link>
+        </li>
+      </ul>
+      <Link href={'/'}>
+        <span className="text-2xl font-bold bg-red-500 py-1 px-2 rounded-lg">
+          Movie
+        </span>
+        <span className="text-2xl font-bold bg-red-500 py-1 px-2 rounded-lg">
+          APP
+        </span>
+      </Link>
+    </div>
+  )
+}
