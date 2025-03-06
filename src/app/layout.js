e8e577afe15d9683e0737dbde327aa98280476de
oztuzelosman <oztuzelosman@gmail.com>
2025-03-06 10:09:36 +0300

    Adding navbar component and top pages route
    Adding navbar component and top pages route

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,6 +2,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import {ClerkProvider} from '@clerk/nextjs'
 import Header from "@/components/Header";
+import Navbar from "@/components/Navbar";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -26,6 +27,7 @@ export default function RootLayout({ children }) {
           className={`${geistSans.variable} ${geistMono.variable} antialiased`}
         >
           <Header/>
+          <Navbar/>
           {children}
         </body>
       </html>
---
+++ src/app/top/[genre]/page.jsx
@@ -0,0 +1,7 @@
+export default function TopPages(){
+  return(
+    <div>
+      Top Pages
+    </div>
+  )
+}
